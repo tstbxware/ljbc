64c54c58af5fcf2264c41960e0408c179cdc126c
ArcticAquila <franklinazriel6942@outlook.com>
2024-05-29 19:51:50 +0700

    feat: Adding empty gradle project

---
+++ src/main/kotlin/com/github/arcticaquila/ljbc/ljbc.kt
@@ -0,0 +1,15 @@
+package com.github.arcticaquila.ljbc
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object ljbc : ModInitializer {
+    private val logger = LoggerFactory.getLogger("ljbc")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
